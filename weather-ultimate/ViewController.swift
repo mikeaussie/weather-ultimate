fb4232f6bd4d1bca23a1c8464768c2fe436ae16e
mikeaussie <michael.pyatin@me.com>
2016-05-06 21:55:38 +1000

    Initial Commit

---
+++ weather-ultimate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather-ultimate
+//
+//  Created by Mike Piatin on 6/05/2016.
+//  Copyright © 2016 Aurora Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
